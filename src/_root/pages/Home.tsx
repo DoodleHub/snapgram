f1dec68ef73de9826e8395246bb563ee26ccd0fa
Chao Chen <chaochen@live.com>
2024-07-12 09:10:06 -0400

    feat: implement home page recent posts

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,5 +1,47 @@
+import { Models } from 'appwrite';
+
+import Loader from '@/components/shared/Loader';
+import { useGetRecentPosts } from '@/lib/react-query/queriesAndMutations';
+import PostCard from '@/components/shared/PostCard';
+
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    data: posts,
+    isPending: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+
+  if (isErrorPosts) {
+    return (
+      <div className="flex flex-1">
+        <div className="home-container">
+          <p className="body-medium text-light-1">Something bad happened</p>
+        </div>
+        <div className="home-creators">
+          <p className="body-medium text-light-1">Something bad happened</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {posts?.documents.map((post: Models.Document) => (
+                <PostCard key={post.$id} post={post} />
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
