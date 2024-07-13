491dc577492ab07f4b1ffe369fabe40835558c2b
Chao Chen <chaochen@live.com>
2024-07-12 23:18:35 -0400

    fix: unable to reach sign up page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,9 @@
-import { getCurrentUser } from '@/lib/appwrite/api';
-import { IContextType, IUser } from '@/types';
 import { createContext, useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { getCurrentUser } from '@/lib/appwrite/api';
+
+import { IContextType, IUser } from '@/types';
 
 export const INITIAL_USER = {
   id: '',
@@ -25,18 +27,25 @@ const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   const [user, setUser] = useState<IUser>(INITIAL_USER);
   const [isLoading, setIsLoading] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     const cookieFallback = localStorage.getItem('cookieFallback');
-    if (cookieFallback === '[]') {
+
+    if (
+      (cookieFallback === '[]' ||
+        cookieFallback === null ||
+        cookieFallback === undefined) &&
+      pathname !== '/sign-up'
+    ) {
       navigate('/sign-in');
     }
 
     checkAuthUser();
-  }, [navigate]);
+  }, [navigate, pathname]);
 
   const checkAuthUser = async () => {
     try {
